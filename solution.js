76e9e117e4bacb4de26dc5121d76cb319d350ce9
adorias <dorias.ahmed@gmail.com>
2022-01-22 14:12:49 -0500

    Completed puzzle and passed tests.
    Puzzle completed using initial values provided.

--- solution.js
+++ solution.js
@@ -1,3 +1,46 @@
 module.exports = function() {
     return "Hello World";
 }
+
+let depthMeasurements = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263
+];
+
+let increaseCount = 0;
+
+function compareMeasurements(currentMeasurement, previousMeasurement, index) {
+    if(index > 0) {
+        if(currentMeasurement > previousMeasurement) {
+            increaseCount++;
+            console.log(currentMeasurement + " (increased)");
+            return 'Increased';
+        } else if(currentMeasurement < previousMeasurement) {
+            console.log(currentMeasurement + " (decreased)");
+            return 'Decreased';
+        } else {
+            console.log(currentMeasurement + " (no change)");
+            return 'No change';
+        }
+    } else {
+        console.log(currentMeasurement + " (N/A - no previous measurement)");
+        return 'No previous measurement';
+    } 
+}
+
+depthMeasurements.forEach((currentMeasurement, index) => {
+    let previousMeasurement = depthMeasurements[index - 1];
+    compareMeasurements(currentMeasurement, previousMeasurement, index);
+});
+
+console.log("Total number of measurement increases: " + increaseCount);
+
+module.exports = compareMeasurements;
--- test/solution_test.js
+++ test/solution_test.js
@@ -1,8 +1,38 @@
-var assert = require('chai').assert;
-var solution = require('../solution');
+let assert = require('chai').assert;
+let compareMeasurements = require('../solution');
 
-describe('Solution', function() {
-    it('solution should return hello world', function() {
-        assert.equal(solution(), 'Hello World');
+describe('Total depth measurement', () => {
+    it('Increased', () => {
+        let currentMeasurement = 205;
+        let previousMeasurement = 200;
+        let index = 3;
+        assert.equal(compareMeasurements(currentMeasurement, previousMeasurement, index), 'Increased');
+    });
+    it('Decreased', () => {
+        let currentMeasurement = 199;
+        let previousMeasurement = 200;
+        let index = 2;
+        assert.equal(compareMeasurements(currentMeasurement, previousMeasurement, index), 'Decreased');
+    });
+    it('Did not change', () => {
+        let currentMeasurement = 200;
+        let previousMeasurement = 200;
+        let index = 1;
+        assert.equal(compareMeasurements(currentMeasurement, previousMeasurement, index), 'No change');
+    });
+    it('has no previous measurement', () => {
+        let currentMeasurement = 200;
+        let previousMeasurement = null;
+        let index = 0;
+        assert.equal(compareMeasurements(currentMeasurement, previousMeasurement, index), 'No previous measurement');
     });
 });
+
+/*
+
+
+
+
+
+
+*/
