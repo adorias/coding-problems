202bd49de60a550ae8a8d922cf13bb1ec7143d23
adorias <dorias.ahmed@gmail.com>
2022-01-22 18:08:41 -0500

    Completed Part 2.
    Note: the aim variable is overloaded in the "forward" test case, and therefore I added a condition in the move() function to account for that.

--- test/solution_test.js
+++ test/solution_test.js
@@ -2,17 +2,17 @@ var assert = require('chai').assert;
 var solution = require('../solution');
 
 describe('Submarine Movement', () => {
-    it('Submarine should move horizontally by X units', () => {
-        let input = 'forward 5';
-        assert.equal(solution.move(input), 'Moved horizontally by 5');
+    it('Submarine should increase aim by 5 units', () => {
+        let movement = 'down 5';
+        assert.equal(solution.move(movement), 'Aim increased by 5');
     });
-    it('Submarine should increase in depth by X units', () => {
-        let input = 'down 5';
-        assert.equal(solution.move(input), 'Increased in depth by 5');
+    it('Submarine should decrease aim by 5 units', () => {
+        let movement = 'up 5';
+        assert.equal(solution.move(movement), 'Aim decreased by 5');
     });
-    it('Submarine should decrease in depth by X units', () => {
-        let input = 'up 3';
-        assert.equal(solution.move(input), 'Decreased in depth by 3');
+    it('Submarine should increase horizontal position by 5 units and increase depth by aim (5) multiplied by 3', () => {
+        let movement = 'forward 3', aim = 5;
+        assert.equal(solution.move(movement, aim), 'Horizontal Postion increased by 3, depth increased by 15');
     });
     it('Final horizontal position and final depth should be multiplied', () => {
         let horizontalPos = 15, depth = 10;
