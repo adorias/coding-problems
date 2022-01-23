7a2f0ca078623d1140fcbe79b98c2baab39ec804
adorias <dorias.ahmed@gmail.com>
2022-01-22 19:54:44 -0500

    Completed part 1.
    Note: in javascript, pushing an element of value 0 into an array doesn't work if done dynamically. Therefore, I found a workaround which does the trick.

--- solution.js
+++ solution.js
@@ -1,28 +1,80 @@
 let diagnosticReport = [
-    00100,
-    11110,
-    10110,
-    10111,
-    10101,
-    01111,
-    00111,
-    11100,
-    10000,
-    11001,
-    00010,
-    01010
+    '00100',
+    '11110',
+    '10110',
+    '10111',
+    '10101',
+    '01111',
+    '00111',
+    '11100',
+    '10000',
+    '11001',
+    '00010',
+    '01010'
 ]
 
-function decodeNumber(binaryNumber) {
+let positionCounters =  new Array(diagnosticReport[0].length).fill(0), gammaRate, epsilonRate, powerConsumption;
 
+function decodeNumber(binaryNumber) {
+    let incrementTracker = 'Incremented at position: ';
+    for (var i = 0; i < binaryNumber.length; i++) {
+        if(binaryNumber.charAt(i) == '1') {
+            positionCounters[i]++;
+            incrementTracker = incrementTracker + i +',';
+        }
+    }
+    return incrementTracker;
+}
+
+function getGammaRate() {
+    for (var i = 0; i < positionCounters.length; i++) {
+        if (i == 0) {
+            if(positionCounters[i] > (diagnosticReport.length / 2)) {
+                gammaRate = '1';
+            } else {
+                gammaRate = '0';
+            }
+        } else {
+            if(positionCounters[i] > (diagnosticReport.length / 2)) {
+                gammaRate = gammaRate + '1';
+            } else {
+                gammaRate = gammaRate + '0';
+            }
+        }
+    }
+}
+
+function getEpsilonRate() {
+    for (var i = 0; i < positionCounters.length; i++) {
+        if (i == 0) {
+            if(positionCounters[i] < (diagnosticReport.length / 2)) {
+                epsilonRate = '1';
+            } else {
+                epsilonRate = '0';
+            }
+        } else {
+            if(positionCounters[i] < (diagnosticReport.length / 2)) {
+                epsilonRate = epsilonRate + '1';
+            } else {
+                epsilonRate = epsilonRate + '0';
+            }
+        }
+    }
 }
 
 function getPowerConsumption(gammaRate, epsilonRate) {
-
+    return powerConsumption = gammaRate * epsilonRate;
 }
+
+diagnosticReport.forEach(binaryNumber => {
+    decodeNumber(binaryNumber);
+});
+
+getGammaRate();
+getEpsilonRate();
+getPowerConsumption(parseInt(gammaRate, 2), parseInt(epsilonRate, 2));
 
 module.exports = {
     decodeNumber,
     getPowerConsumption
 }
-
--- test/solution_test.js
+++ test/solution_test.js
@@ -3,11 +3,11 @@ var solution = require('../solution');
 
 describe('Diagnostic report evaluation', function() {
     it('Is the binary number incrementing when each position is equal to 1', function() {
-        let binaryNumber = 11111;
-        assert.equal(solution.decodeNumber(binaryNumber), 'Incremented at position: 0,1,2,3,4');
+        let binaryNumber = '11111';
+        assert.equal(solution.decodeNumber(binaryNumber), 'Incremented at position: 0,1,2,3,4,');
     });
     it('Is the binary number not incrementing when each position is equal to 0', function() {
-        let binaryNumber = 00000;
+        let binaryNumber = '00000';
         assert.equal(solution.decodeNumber(binaryNumber), 'Incremented at position: ');
     });
     it('Is the power consumption equal to the gamma rate multiplied by the epsilon rate', function() {
