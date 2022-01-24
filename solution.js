62149e8aa24c4ed6db90f78e0001d9da9e1153dd
adorias <dorias.ahmed@gmail.com>
2022-01-23 22:41:16 -0500

    Almost done, need to debug further.

--- solution.js
+++ solution.js
@@ -1,24 +1,122 @@
+class Board {
+    constructor(tileRows) {
+        this.tileRows = tileRows;
+    }
+}
 
+class Tile {
+    constructor(value, marked) {
+        this.value = value;
+        this.marked = marked;
+    }
+}
 
-function nextNumber(index) {
+let randomNumbers = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1];
+let boardsStr =
+`22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7`
+;
+let boardsArr = boardsStr.split(/\s+/), boardsObjs = [], boards = [];
+
+function isWinningBoard(board) {    
+    for(let i = 0; i < board.tileRows.length; i++) {
+        let countRow = 0, countCol = 0;
+        for(let j = 0; j < board.tileRows.length; j++) {
+            if(board.tileRows[i][j].marked == true) {
+                countRow++;
+            }
+            if(board.tileRows[j][i].marked == true) {
+                countCol++;
+            }
+        }
+        // If either the row or the column is fully marked, then it's a winning board.
+        if(countRow == board.tileRows.length || countCol == board.tileRows.length) {
+            return true;
+        }
+    }
+    // Otherwise, this isn't a winning board.
+    return false;
 
 }
 
-function mark(randomNumber, board) {
-
+function getFinalScore(winningNumber, board) {
+    let sum = 0;
+    board.tileRows.forEach(tileRow => {
+        tileRow.forEach(tile => {
+            if(tile.marked == false) {
+                sum += tile.value;
+            }
+        });
+    });
+    console.log('final score');
+    console.log(sum * winningNumber);
+    return (sum * winningNumber);
 }
 
-function isWinningBoard(board) {
+// Turn all array values into tiles.
+boardsArr.forEach(tile => {
+    boardsObjs.push(new Tile(tile, false));
+});
 
+// Separate tiles into Boards.
+while(boardsObjs.length) {
+    boards.push(new Board(boardsObjs.splice(0, 25)));
 }
 
-function getFinalScore(randomNumber, board) {
+// Turn 1D boards into 2D boards. (easier manipulation).
+boards.forEach(board => {
+    let tempArr = [];
+    while(board.tileRows.length) {
+        tempArr.push(board.tileRows.splice(0, 5));
+    }
+    board.tileRows = tempArr;
+});
 
-}
+// 
+randomNumbers.forEach(randomNumber => {
+    console.log('RandomNumber being generated')
+    // For each board
+    boards.forEach(board => {
+        console.log('board')
+        // Mark the tile when the number is found (if available)
+        board.tileRows.forEach(tileRow => {
+            tileRow.forEach(tile => {
+                // If the tile has the same value as the random number
+                if(tile.value == randomNumber) {
+                    console.log('tile is random number');
+                    // Mark the tile
+                    tile.marked = true;
+                    // Check if the board won
+                    if(isWinningBoard(board)) {
+                        // Get the final score
+                        getFinalScore(randomNumber, board)
+                        //TODO: Break
+                        console.log("I got the final score!");
+                        //return false;
+                    }
+                }
+            })
+        });
+    });
+    //return true;
+});
 
 module.exports = {
-    nextNumber,
-    mark,
     isWinningBoard,
     getFinalScore
 }
--- test/solution_test.js
+++ test/solution_test.js
@@ -2,11 +2,6 @@ var assert = require('chai').assert;
 var solution = require('../solution');
 
 describe('Turn', function() {
-    it('Is the chosen number called sequentially', function() {
-        let randomNumbers = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1];
-        let index = 2;
-        assert.equal(solution.nextNumber(index), 9);
-    });
     it('Is the number marked on the board that contains it', function() {
         let randomNumber = 9;
         let board = [
