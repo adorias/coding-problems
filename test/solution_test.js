5cb3847f8963e9b393453c235a8731fb1b23aa0a
adorias <dorias.ahmed@gmail.com>
2022-05-11 07:38:41 -0400

    Completed part 1.

--- test/solution_test.js
+++ test/solution_test.js
@@ -2,7 +2,16 @@ var assert = require('chai').assert;
 var solution = require('../solution');
 
 describe('Solution', function() {
-    it('solution should return hello world', function() {
-        assert.equal(solution(), 'Hello World');
+    it('should decrement all lanternfish by 1 everyday', function() {
+        const inputs = [1,6,4,2];  
+        const expected = [0,5,3,1];
+        const actual = solution.processADay(inputs);
+        assert.deepEqual(actual, expected);
+    });
+    it('should create a new lanternfish when a lanternfish passes 0, and the value should reset to 6.', function() {
+      const inputs = [0,6,4,2];
+      const expected = [6,5,3,1,8];
+      const actual = solution.processADay(inputs);
+      assert.deepEqual(actual, expected);
     });
 });
