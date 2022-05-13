8fb307fb189667b1624fbca63314178c26983e86
adorias <dorias.ahmed@gmail.com>
2022-05-12 22:39:00 -0400

    Completed part 2.

--- solution.js
+++ solution.js
@@ -4,20 +4,47 @@ module.exports = {
 
 let inputs = [4,1,1,1,5,1,3,1,5,3,4,3,3,1,3,3,1,5,3,2,4,4,3,4,1,4,2,2,1,3,5,1,1,3,2,5,1,1,4,2,5,4,3,2,5,3,3,4,5,4,3,5,4,2,5,5,2,2,2,3,5,5,4,2,1,1,5,1,4,3,2,2,1,2,1,5,3,3,3,5,1,5,4,2,2,2,1,4,2,5,2,3,3,2,3,4,4,1,4,4,3,1,1,1,1,1,4,4,5,4,2,5,1,5,4,4,5,2,3,5,4,1,4,5,2,1,1,2,5,4,5,5,1,1,1,1,1,4,5,3,1,3,4,3,3,1,5,4,2,1,4,4,4,1,1,3,1,3,5,3,1,4,5,3,5,1,1,2,2,4,4,1,4,1,3,1,1,3,1,3,3,5,4,2,1,1,2,1,2,3,3,5,4,1,1,2,1,2,5,3,1,5,4,3,1,5,2,3,4,4,3,1,1,1,2,1,1,2,1,5,4,2,2,1,4,3,1,1,1,1,3,1,5,2,4,1,3,2,3,4,3,4,2,1,2,1,2,4,2,1,5,2,2,5,5,1,1,2,3,1,1,1,3,5,1,3,5,1,3,3,2,4,5,5,3,1,4,1,5,2,4,5,5,5,2,4,2,2,5,2,4,1,3,2,1,1,4,4,1,5];
 
-function processADay(inputs) {
-    inputs.forEach((element, index) => {
-        if(element > 0) {
-            inputs[index] = inputs[index] - 1;
-        } else {
-            inputs[index] = 6;
-            inputs.push(8);
+let fishMap = new Map();
+
+inputs.forEach(element => {
+    if(fishMap.has(element)) {
+        fishMap.set(element, fishMap.get(element) + 1);
+    } else {
+        fishMap.set(element, 1);
+    }
+})
+
+function processADay(fishMap) {
+    let tempMap = new Map();
+    
+    for(let i = 0; i <= 8; i++) {
+        if(fishMap.has(i)) {
+            tempMap.set((i - 1), fishMap.get(i));
         }
-    })
-    return inputs;
+    }
+
+    if(tempMap.has(-1)) {
+        if(tempMap.has(6)) {
+            tempMap.set(6, tempMap.get(6) + tempMap.get(-1));
+        } else {
+            tempMap.set(6, tempMap.get(-1));
+        }
+        tempMap.set(8, tempMap.get(-1));
+        tempMap.delete(-1);
+    }
+
+    return tempMap;
 }
 
-for(let i = 0; i < 80; i++) {
-    processADay(inputs);
+for(let i = 0; i < 256; i++) {
+    fishMap = processADay(fishMap);
 }
 
-console.log(inputs.length);
+console.log(fishMap);
+
+let count = 0;
+for(let i = 0; i <= 8; i++) {
+    count += fishMap.get(i);
+}
+
+console.log(count);
--- test/solution_test.js
+++ test/solution_test.js
@@ -3,15 +3,30 @@ var solution = require('../solution');
 
 describe('Solution', function() {
     it('should decrement all lanternfish by 1 everyday', function() {
-        const inputs = [1,6,4,2];  
-        const expected = [0,5,3,1];
-        const actual = solution.processADay(inputs);
+        let input = new Map(), expected = new Map();
+        input.set(1, 1);
+        input.set(6, 1);
+        input.set(4, 1);
+        input.set(2, 1);
+        expected.set(0, 1);
+        expected.set(5, 1);
+        expected.set(3, 1);
+        expected.set(1, 1);
+        const actual = solution.processADay(input);
         assert.deepEqual(actual, expected);
     });
     it('should create a new lanternfish when a lanternfish passes 0, and the value should reset to 6.', function() {
-      const inputs = [0,6,4,2];
-      const expected = [6,5,3,1,8];
-      const actual = solution.processADay(inputs);
+      let input = new Map(), expected = new Map();
+      input.set(0, 1);
+      input.set(6, 1);
+      input.set(4, 1);
+      input.set(2, 1);
+      expected.set(6, 1);
+      expected.set(5, 1);
+      expected.set(3, 1);
+      expected.set(1, 1);
+      expected.set(8, 1);
+      const actual = solution.processADay(input);
       assert.deepEqual(actual, expected);
     });
 });
